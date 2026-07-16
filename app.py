7a62d2ad494a631e77c0cfa516499b7e03e44d0a
elisagenari <draelisagenaris@gmail.com>
2026-07-15 21:25:30 -0300

    Projeto finalizado

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+#Cabeçalho
+st.header("Dashboard de Veículos")
+
+#Ler os dados
+df = pd.read_csv('vehicles.csv')
+
+#Botão do histograma
+if st.button("Criar histograma"):
+    st.write("Histograma da quilometragem dos veículos")
+    fig = px.histogram(df, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+#Botão do gráfico de dispersão
+if st.button("Criar gráfico de dispersão"):
+    st.write("Relação entre preço e quilometragem")
+    fig = px.scatter(df, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
